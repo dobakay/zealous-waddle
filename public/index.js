1cf743c6d8d45a13b9404362dc79d263a7edf8f3
Putzmitt3l <dkralev90@gmail.com>
2015-09-17 17:59:27 +0300

    Adding simple styling and markup

--- public/index.js
+++ public/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var App = angular.module('App', []);
 
 App.controller('TableCtrl', function($scope, $http) {
@@ -6,5 +8,21 @@ App.controller('TableCtrl', function($scope, $http) {
 			$scope.table = res.data;
 			$scope.tableColumnNames = res.data['columns'];
 			$scope.tableRecords = res.data['records'];
+
+			$scope.currentPage = res.currentPage;
+			$scope.pageSize = res.recordsPerPage;
+
+			$scope.order = res.orderBy;
+
+			$scope.numberOfPages=function(){
+					return Math.ceil($scope.tableRecords.length/$scope.pageSize);
+			}
 		});
+});
+
+App.filter('startFrom', function() {
+		return function(input, start) {
+				start = +start; //parse to int
+				return input.slice(start);
+		}
 });
