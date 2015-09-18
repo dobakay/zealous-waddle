04fd449edc0df5676df7967a94fb92b1006b3e8d
Putzmitt3l <dkralev90@gmail.com>
2015-09-18 17:19:19 +0300

    Fixing binding data to scope

--- public/index.js
+++ public/index.js
@@ -1,18 +1,18 @@
-'use strict';
+(function () {
+	'use strict';
 
 var App = angular.module('App', []);
 
 App.controller('TableCtrl', function($scope, $http) {
 	$http.get('table.json')
 		.then(function(res){
-			$scope.table = res.data;
 			$scope.tableColumnNames = res.data['columns'];
 			$scope.tableRecords = res.data['records'];
 
-			$scope.currentPage = res.currentPage;
-			$scope.pageSize = res.recordsPerPage;
+			$scope.currentPage = res.data['currentPage'];
+			$scope.pageSize = res.data['recordsPerPage'];
 
-			$scope.order = res.orderBy;
+			$scope.order = res.data['orderBy'];
 
 			$scope.numberOfPages=function(){
 					return Math.ceil($scope.tableRecords.length/$scope.pageSize);
@@ -26,3 +26,5 @@ App.filter('startFrom', function() {
 				return input.slice(start);
 		}
 });
+
+})();
